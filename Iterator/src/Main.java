fef294997d7a630344208944c6d27bd89dcc1012
Chloe Mai <114679264+chloemaivu@users.noreply.github.com>
2023-10-24 15:35:10 +0100

    Add files via upload

--- Iterator/src/Main.java
+++ Iterator/src/Main.java
@@ -1,4 +1,3 @@
-import java.io.*;
 import java.lang.*;
 import java.util.*;
 
@@ -8,38 +7,21 @@ public class Main {
         players.put("Sterling", 10);
         players.put("Rashford", 11);
         players.put("Sancho", 7);
+        players.put("Saka", 25);
 
-//        players.put(new Player("Saka", 25), 25);
-//        players.put(new Player("Rashford", 11), 11);
-//        players.put(new Player("Sancho", 7), 7);
-//        players.put(new Player("Sterling", 10), 10);
-
-//        System.out.println("Unsorted: ");
-//        System.out.println(players);
-//
-//        Collections.sort(List<players>, new NumSort());
-//
-//        System.out.println("Sorted by Player Number: ");
-//        System.out.println(players);
-//
-////        Collections.sort(players, new NameSort());
-//
-//        System.out.println("Sorted by Player Name: ");
-//        System.out.println(players);
-
+        System.out.println("Unsorted: ");
         Set set = players.entrySet();
-        Iterator iterator = set.iterator();
-        while (iterator.hasNext()) {
-            Map.Entry map = (Map.Entry) iterator.next();
+        for (Object o : set) {
+            Map.Entry map = (Map.Entry) o;
             System.out.println("Name:  " + map.getKey() + "     Number:   " + map.getValue());
         }
-        Map<Integer, String> map = sortValues(players);
+        HashMap map = sortValues(players);
         System.out.println("\n");
+
         System.out.println("After Sorting by Squad Number:");
         Set set2 = map.entrySet();
-        Iterator iterator2 = set2.iterator();
-        while (iterator2.hasNext()) {
-            Map.Entry me2 = (Map.Entry) iterator2.next();
+        for (Object o : set2) {
+            Map.Entry me2 = (Map.Entry) o;
             System.out.println("Name:  " + me2.getKey() + "     Number:   " + me2.getValue());
         }
     }
@@ -47,13 +29,13 @@ public class Main {
     //method to sort values
     private static HashMap sortValues(HashMap map) {
         List list = new LinkedList(map.entrySet());
-//Custom Comparator
+        //Custom Comparator
         Collections.sort(list, new Comparator() {
             public int compare(Object o1, Object o2) {
                 return ((Comparable) ((Map.Entry) (o1)).getValue()).compareTo(((Map.Entry) (o2)).getValue());
             }
         });
-//copying the sorted list in HashMap to preserve the iteration order
+        //copying the sorted list in HashMap to preserve the iteration order
         HashMap sortedHashMap = new LinkedHashMap();
         for (Iterator it = list.iterator(); it.hasNext(); ) {
             Map.Entry entry = (Map.Entry) it.next();
